2d90348a138d371cae6f1680504390ed905db316
STEVENSONSJ22 <StevensonSJ22@gcc.edu>
2025-02-14 10:31:46 -0500

    Implemented code stubs for the account class.

--- src/main/java/TODO/Account.java
+++ src/main/java/TODO/Account.java
@@ -1,9 +1,64 @@
 package TODO;
 
+import java.util.ArrayList;
+
 public class Account {
     // Variables
+    private ArrayList<Class> classesTaken;
+    private ArrayList<Schedule> schedules;
+    private String username;
+    private String password;
+    private String email;
+    private ArrayList<String> majors;
+    private ArrayList<String> minors;
 
     // Constructor
+    public Account (String username, String password, String email, ArrayList<String> majors, ArrayList<String> minors) {
+        //TODO: Implement Constructor
+    }
 
     // Methods
+    public void addTakenClass(Class c) {
+        //TODO: Implement addTakenClass
+    }
+    public void removeTakenClass(Class c){
+        //TODO: Implement removeTakenClass
+    }
+
+    public String getUsername(){
+        //TODO: Implement getUsername
+        return null;
+    }
+    public void setUsername(String username){
+        //TODO: Implement setUsername
+    }
+
+    public void setPassword(String password){
+        //TODO: Implement setPassword
+    }
+
+    public void setEmail(String email){
+        //TODO: Implement setEmail
+    }
+
+    public void addSchedule(String name){
+        //TODO: Implement addSchedule
+    }
+    public void removeSchedule(String name){
+        //TODO: Implement removeSchedule
+    }
+
+    public void addMajor(String major){
+        //TODO: Implement addMajor
+    }
+    public void removeMajor(String major){
+        //TODO: Implement removeMajor
+    }
+
+    public void addMinor(String minor){
+        //TODO: Implement addMinor
+    }
+    public void removeMinor(String minor){
+        //TODO: Implement removeMinor
+    }
 }
