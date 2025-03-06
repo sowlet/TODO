990bea7eddcf10fe7cefabc9e5a1a91e9641dab8
bennalex2014 <alexander.j.bennett3@gmail.com>
2025-03-06 18:07:22 -0500

    Created JUnit test class for Search
    commented Search class methods
    
    removed modifySearch method due to redundancy
    
    base implementation for search

---
+++ src/test/java/TODO/SearchTest.java
@@ -0,0 +1,16 @@
+package TODO;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class SearchTest {
+
+    @Test
+    void search() {
+    }
+
+    @Test
+    void modifyFilter() {
+    }
+}
