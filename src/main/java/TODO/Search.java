05d6a0562fa912c4ca40ffcd444909caa6075b61
bennalex2014 <alexander.j.bennett3@gmail.com>
2025-03-06 18:07:22 -0500

    Created JUnit test class for Search
    commented Search class methods
    
    removed modifySearch method due to redundancy
    
    base implementation for search

--- src/main/java/TODO/Search.java
+++ src/main/java/TODO/Search.java
@@ -3,13 +3,13 @@ package TODO;
 import java.util.ArrayList;
 
 public class Search {
-    // Variables
     private ArrayList<Class> searchResults;
     private String query;
     private ArrayList<Filter> filters;
     private ArrayList<Class> filterResults;
 
-    // Constructor
+
+    // Class constructor
     public Search(){
         searchResults = new ArrayList<>();
         query = "";
@@ -17,17 +17,45 @@ public class Search {
         filterResults = new ArrayList<>();
     }
 
-    // Methods
-    public ArrayList<Class> search(String query) {
-        //TODO: Implement search
-        return null;
+    /*
+    searches through classes and returns an ArrayList of results based on query and stored filters
+
+    @param query the name of the class requested
+    @param classes the global ArrayList of classes
+    @return an ArrayList of matching classes that pass the stored filters
+     */
+    public ArrayList<Class> search(String query, ArrayList<Class> classes) {
+        this.query = query;
+
+        for (Class c: classes) {
+            if (c.getName().contains(query)) {
+                searchResults.add(c);
+            }
+        }
+
+        filterResults = searchResults;
+
+        for (Filter f: filters) {
+            filterResults = f.filt(filterResults);
+        }
+
+        return filterResults;
     }
 
-    public ArrayList<Class> modifyQuery(String query) {
+
+    /* redundant method -> search can be used to modify the query
+    public ArrayList<Class> modifyQuery(String query, ArrayList<Class> classes) {
         //TODO: Implement modifyQuery
         return null;
-    }
+    }*/
 
+
+    /*
+    adds Filter object to filters ArrayList, replacing Filter subclasses if there are duplicate types
+
+    @param filter the Filter object to be added
+    @return new results with updated Filters
+     */
     public ArrayList<Class> modifyFilter(Filter filter){
         //TODO: Implement modifyFilter
         return null;
