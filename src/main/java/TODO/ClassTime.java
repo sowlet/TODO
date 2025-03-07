36d5f1760ac9a7b0ec72f732355f1a7494668b6c
bennalex2014 <alexander.j.bennett3@gmail.com>
2025-03-06 20:39:38 -0500

    Changed start_time and end_time to String type to allow for JSON parsing

--- src/main/java/TODO/ClassTime.java
+++ src/main/java/TODO/ClassTime.java
@@ -4,10 +4,10 @@ import java.sql.Time;
 
 public class ClassTime {
     private String day;
-    private Time end_time;
-    private Time start_time;
+    private String end_time;
+    private String start_time;
 
-    public ClassTime(String day, Time end_time, Time start_time){
+    public ClassTime(String day, String end_time, String start_time){
         this.day = day;
         this.end_time = end_time;
         this.start_time = start_time;
@@ -17,11 +17,11 @@ public class ClassTime {
         return day;
     }
 
-    public Time getEndTime(){
+    public String getEndTime(){
         return end_time;
     }
 
-    public Time getStartTime(){
+    public String getStartTime(){
         return start_time;
     }
 }
