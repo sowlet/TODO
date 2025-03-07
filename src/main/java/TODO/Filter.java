5d5b1ab459200ba948855256f6458ebf404ffc95
bennalex2014 <alexander.j.bennett3@gmail.com>
2025-03-06 19:36:27 -0500

    commented Filter family
    base implementation to DepartmentFilter

--- src/main/java/TODO/Filter.java
+++ src/main/java/TODO/Filter.java
@@ -2,29 +2,44 @@ package TODO;
 
 import java.util.ArrayList;
 
+// default Filter
 abstract class Filter {
-
-    public Filter(){
-        //TODO
-    }
-
+    public Filter(){} // empty constructor
     public abstract ArrayList<Class> filt(ArrayList<Class> classes);
 }
 
+
+// filters by department i.e. subject
 class DepartmentFilter extends Filter {
     private String dept;
 
+    // constructor
     public void DepartmentFilter(String dept) {
-        //TODO: Implement filter by department
+        this.dept = dept.toUpperCase(); // all subjects are upper case
     }
 
+    /*
+    filters by list of classes by internally stored department String
+
+    @param classes list of classes to be filtered
+    @return list of classes that match the department
+     */
     @Override
     public ArrayList<Class> filt(ArrayList<Class> classes) {
-        //TODO
-        return null;
+        ArrayList<Class> fClasses = new ArrayList<>();
+
+        for (Class c: classes) {
+            if (c.getSubject().equals(dept)) {
+                fClasses.add(c);
+            }
+        }
+
+        return fClasses;
     }
 }
 
+
+// filters by Date
 class DateFilter extends Filter {
     private Day days;
 
@@ -39,11 +54,14 @@ class DateFilter extends Filter {
     }
 }
 
+// Day options for classes
 enum Day {
     MWF,
     TR;
 }
 
+
+// filters by time
 class TimeFilter extends Filter {
     private String start;
     private String end;
