f04539cdb968c426f6ab66fb9ce7015841e67c10
bennalex2014 <alexander.j.bennett3@gmail.com>
2025-03-11 16:05:49 -0400

    created stub tests for Class giveRating and hasTimeConflict

--- src/test/java/TODO/ClassTest.java
+++ src/test/java/TODO/ClassTest.java
@@ -1,7 +1,16 @@
 package TODO;
 
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class ClassTest {
 
+    @Test
+    void giveRating() {
+    }
+
+    @Test
+    void hasTimeConflict() {
+    }
 }
