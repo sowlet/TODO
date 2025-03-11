cdcecd693cf47dee385a200126ff0802c194edcf
bennalex2014 <alexander.j.bennett3@gmail.com>
2025-03-11 16:54:18 -0400

    timeConflict tests

--- src/test/java/TODO/ClassTest.java
+++ src/test/java/TODO/ClassTest.java
@@ -137,14 +137,294 @@ class ClassTest {
 
 
     @Test
-    void endsInOtherConflict() {
+    void overlapByOneMinuteConflict() {
+        ArrayList<Class> sampleClasses = new ArrayList<>();
+
+        // small portion of JSON data
+        // TR
+        String json1 = "{\n" +
+                "            \"credits\": 3,\n" +
+                "            \"faculty\": [\n" +
+                "                \"McFeaters, Michelle R.\"\n" +
+                "            ],\n" +
+                "            \"is_lab\": false,\n" +
+                "            \"is_open\": true,\n" +
+                "            \"location\": \"SHAL 314\",\n" +
+                "            \"name\": \"COST ACCOUNTING\",\n" +
+                "            \"number\": 303,\n" +
+                "            \"open_seats\": 2,\n" +
+                "            \"section\": \"A\",\n" +
+                "            \"semester\": \"2023_Fall\",\n" +
+                "            \"subject\": \"ACCT\",\n" +
+                "            \"times\": [\n" +
+                "                {\n" +
+                "                    \"day\": \"T\",\n" +
+                "                    \"end_time\": \"09:30:00\",\n" +
+                "                    \"start_time\": \"08:00:00\"\n" +
+                "                },\n" +
+                "                {\n" +
+                "                    \"day\": \"R\",\n" +
+                "                    \"end_time\": \"09:30:00\",\n" +
+                "                    \"start_time\": \"08:00:00\"\n" +
+                "                }\n" +
+                "            ],\n" +
+                "            \"total_seats\": 30\n" +
+                "        }";
+
+        // TR
+        String json3 = "{\n" +
+                "            \"credits\": 3,\n" +
+                "            \"faculty\": [\n" +
+                "                \"Shultz, Tricia Michele\"\n" +
+                "            ],\n" +
+                "            \"is_lab\": false,\n" +
+                "            \"is_open\": true,\n" +
+                "            \"location\": \"SHAL 308\",\n" +
+                "            \"name\": \"ADVANCED ACCOUNTING II\",\n" +
+                "            \"number\": 402,\n" +
+                "            \"open_seats\": 10,\n" +
+                "            \"section\": \"A\",\n" +
+                "            \"semester\": \"2023_Fall\",\n" +
+                "            \"subject\": \"ACCT\",\n" +
+                "            \"times\": [\n" +
+                "                {\n" +
+                "                    \"day\": \"T\",\n" +
+                "                    \"end_time\": \"10:45:00\",\n" +
+                "                    \"start_time\": \"09:30:00\"\n" +
+                "                },\n" +
+                "                {\n" +
+                "                    \"day\": \"R\",\n" +
+                "                    \"end_time\": \"10:45:00\",\n" +
+                "                    \"start_time\": \"09:30:00\"\n" +
+                "                }\n" +
+                "            ],\n" +
+                "            \"total_seats\": 25\n" +
+                "        }";
+        String[] js = {json1,json3};
+        for (String j: js) {
+            sampleClasses.add(new Gson().fromJson(j,Class.class));
+        }
+
+        assertEquals(true,sampleClasses.get(0).hasTimeConflict(sampleClasses.get(1)));
+        assertEquals(true,sampleClasses.get(1).hasTimeConflict(sampleClasses.get(0)));
     }
 
     @Test
-    void startInOtherConflict() {
+    void largerOverlapConflict() {
+        ArrayList<Class> sampleClasses = new ArrayList<>();
+
+        // small portion of JSON data
+        // TR
+        String json1 = "{\n" +
+                "            \"credits\": 3,\n" +
+                "            \"faculty\": [\n" +
+                "                \"McFeaters, Michelle R.\"\n" +
+                "            ],\n" +
+                "            \"is_lab\": false,\n" +
+                "            \"is_open\": true,\n" +
+                "            \"location\": \"SHAL 314\",\n" +
+                "            \"name\": \"COST ACCOUNTING\",\n" +
+                "            \"number\": 303,\n" +
+                "            \"open_seats\": 2,\n" +
+                "            \"section\": \"A\",\n" +
+                "            \"semester\": \"2023_Fall\",\n" +
+                "            \"subject\": \"ACCT\",\n" +
+                "            \"times\": [\n" +
+                "                {\n" +
+                "                    \"day\": \"T\",\n" +
+                "                    \"end_time\": \"11:30:00\",\n" +
+                "                    \"start_time\": \"10:00:00\"\n" +
+                "                },\n" +
+                "                {\n" +
+                "                    \"day\": \"R\",\n" +
+                "                    \"end_time\": \"11:30:00\",\n" +
+                "                    \"start_time\": \"10:00:00\"\n" +
+                "                }\n" +
+                "            ],\n" +
+                "            \"total_seats\": 30\n" +
+                "        }";
+
+        // TR
+        String json3 = "{\n" +
+                "            \"credits\": 3,\n" +
+                "            \"faculty\": [\n" +
+                "                \"Shultz, Tricia Michele\"\n" +
+                "            ],\n" +
+                "            \"is_lab\": false,\n" +
+                "            \"is_open\": true,\n" +
+                "            \"location\": \"SHAL 308\",\n" +
+                "            \"name\": \"ADVANCED ACCOUNTING II\",\n" +
+                "            \"number\": 402,\n" +
+                "            \"open_seats\": 10,\n" +
+                "            \"section\": \"A\",\n" +
+                "            \"semester\": \"2023_Fall\",\n" +
+                "            \"subject\": \"ACCT\",\n" +
+                "            \"times\": [\n" +
+                "                {\n" +
+                "                    \"day\": \"T\",\n" +
+                "                    \"end_time\": \"10:45:00\",\n" +
+                "                    \"start_time\": \"09:30:00\"\n" +
+                "                },\n" +
+                "                {\n" +
+                "                    \"day\": \"R\",\n" +
+                "                    \"end_time\": \"10:45:00\",\n" +
+                "                    \"start_time\": \"09:30:00\"\n" +
+                "                }\n" +
+                "            ],\n" +
+                "            \"total_seats\": 25\n" +
+                "        }";
+        String[] js = {json1,json3};
+        for (String j: js) {
+            sampleClasses.add(new Gson().fromJson(j,Class.class));
+        }
+
+        assertEquals(true,sampleClasses.get(0).hasTimeConflict(sampleClasses.get(1)));
+        assertEquals(true,sampleClasses.get(1).hasTimeConflict(sampleClasses.get(0)));
     }
 
     @Test
-    void noConflict() {
+    void noConflictSameDay() {
+        ArrayList<Class> sampleClasses = new ArrayList<>();
+
+        // small portion of JSON data
+        // TR
+        String json1 = "{\n" +
+                "            \"credits\": 3,\n" +
+                "            \"faculty\": [\n" +
+                "                \"McFeaters, Michelle R.\"\n" +
+                "            ],\n" +
+                "            \"is_lab\": false,\n" +
+                "            \"is_open\": true,\n" +
+                "            \"location\": \"SHAL 314\",\n" +
+                "            \"name\": \"COST ACCOUNTING\",\n" +
+                "            \"number\": 303,\n" +
+                "            \"open_seats\": 2,\n" +
+                "            \"section\": \"A\",\n" +
+                "            \"semester\": \"2023_Fall\",\n" +
+                "            \"subject\": \"ACCT\",\n" +
+                "            \"times\": [\n" +
+                "                {\n" +
+                "                    \"day\": \"T\",\n" +
+                "                    \"end_time\": \"09:15:00\",\n" +
+                "                    \"start_time\": \"08:00:00\"\n" +
+                "                },\n" +
+                "                {\n" +
+                "                    \"day\": \"R\",\n" +
+                "                    \"end_time\": \"09:15:00\",\n" +
+                "                    \"start_time\": \"08:00:00\"\n" +
+                "                }\n" +
+                "            ],\n" +
+                "            \"total_seats\": 30\n" +
+                "        }";
+
+        // TR
+        String json3 = "{\n" +
+                "            \"credits\": 3,\n" +
+                "            \"faculty\": [\n" +
+                "                \"Shultz, Tricia Michele\"\n" +
+                "            ],\n" +
+                "            \"is_lab\": false,\n" +
+                "            \"is_open\": true,\n" +
+                "            \"location\": \"SHAL 308\",\n" +
+                "            \"name\": \"ADVANCED ACCOUNTING II\",\n" +
+                "            \"number\": 402,\n" +
+                "            \"open_seats\": 10,\n" +
+                "            \"section\": \"A\",\n" +
+                "            \"semester\": \"2023_Fall\",\n" +
+                "            \"subject\": \"ACCT\",\n" +
+                "            \"times\": [\n" +
+                "                {\n" +
+                "                    \"day\": \"T\",\n" +
+                "                    \"end_time\": \"10:45:00\",\n" +
+                "                    \"start_time\": \"09:30:00\"\n" +
+                "                },\n" +
+                "                {\n" +
+                "                    \"day\": \"R\",\n" +
+                "                    \"end_time\": \"10:45:00\",\n" +
+                "                    \"start_time\": \"09:30:00\"\n" +
+                "                }\n" +
+                "            ],\n" +
+                "            \"total_seats\": 25\n" +
+                "        }";
+        String[] js = {json1,json3};
+        for (String j: js) {
+            sampleClasses.add(new Gson().fromJson(j,Class.class));
+        }
+
+        assertEquals(false,sampleClasses.get(0).hasTimeConflict(sampleClasses.get(1)));
+        assertEquals(false,sampleClasses.get(1).hasTimeConflict(sampleClasses.get(0)));
+    }
+
+    @Test
+    void noConflictAltDay() {
+        ArrayList<Class> sampleClasses = new ArrayList<>();
+
+        // small portion of JSON data
+        // MW
+        String json1 = "{\n" +
+                "            \"credits\": 3,\n" +
+                "            \"faculty\": [\n" +
+                "                \"McFeaters, Michelle R.\"\n" +
+                "            ],\n" +
+                "            \"is_lab\": false,\n" +
+                "            \"is_open\": true,\n" +
+                "            \"location\": \"SHAL 314\",\n" +
+                "            \"name\": \"COST ACCOUNTING\",\n" +
+                "            \"number\": 303,\n" +
+                "            \"open_seats\": 2,\n" +
+                "            \"section\": \"A\",\n" +
+                "            \"semester\": \"2023_Fall\",\n" +
+                "            \"subject\": \"ACCT\",\n" +
+                "            \"times\": [\n" +
+                "                {\n" +
+                "                    \"day\": \"M\",\n" +
+                "                    \"end_time\": \"09:15:00\",\n" +
+                "                    \"start_time\": \"08:00:00\"\n" +
+                "                },\n" +
+                "                {\n" +
+                "                    \"day\": \"W\",\n" +
+                "                    \"end_time\": \"09:15:00\",\n" +
+                "                    \"start_time\": \"08:00:00\"\n" +
+                "                }\n" +
+                "            ],\n" +
+                "            \"total_seats\": 30\n" +
+                "        }";
+
+        // TR
+        String json3 = "{\n" +
+                "            \"credits\": 3,\n" +
+                "            \"faculty\": [\n" +
+                "                \"Shultz, Tricia Michele\"\n" +
+                "            ],\n" +
+                "            \"is_lab\": false,\n" +
+                "            \"is_open\": true,\n" +
+                "            \"location\": \"SHAL 308\",\n" +
+                "            \"name\": \"ADVANCED ACCOUNTING II\",\n" +
+                "            \"number\": 402,\n" +
+                "            \"open_seats\": 10,\n" +
+                "            \"section\": \"A\",\n" +
+                "            \"semester\": \"2023_Fall\",\n" +
+                "            \"subject\": \"ACCT\",\n" +
+                "            \"times\": [\n" +
+                "                {\n" +
+                "                    \"day\": \"T\",\n" +
+                "                    \"end_time\": \"10:45:00\",\n" +
+                "                    \"start_time\": \"09:30:00\"\n" +
+                "                },\n" +
+                "                {\n" +
+                "                    \"day\": \"R\",\n" +
+                "                    \"end_time\": \"10:45:00\",\n" +
+                "                    \"start_time\": \"09:30:00\"\n" +
+                "                }\n" +
+                "            ],\n" +
+                "            \"total_seats\": 25\n" +
+                "        }";
+        String[] js = {json1,json3};
+        for (String j: js) {
+            sampleClasses.add(new Gson().fromJson(j,Class.class));
+        }
+
+        assertEquals(false,sampleClasses.get(0).hasTimeConflict(sampleClasses.get(1)));
+        assertEquals(false,sampleClasses.get(1).hasTimeConflict(sampleClasses.get(0)));
     }
 }
