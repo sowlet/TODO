d31979df2bd513ce04c2ffc6ecc0eb6436388538
bennalex2014 <alexander.j.bennett3@gmail.com>
2025-03-11 19:13:16 -0400

    testing times past noon

--- src/test/java/TODO/TimeFilterTest.java
+++ src/test/java/TODO/TimeFilterTest.java
@@ -123,7 +123,114 @@ class TimeFilterTest {
     }
 
     @Test
-    void moreTests() {
+    void past12Test() {
+        ArrayList<Class> sampleClasses = new ArrayList<>();
+
+        // small poriton of JSON data
+        // TR
+        String json1 = "{\n" +
+                "            \"credits\": 3,\n" +
+                "            \"faculty\": [\n" +
+                "                \"Graybill, Keith B.\"\n" +
+                "            ],\n" +
+                "            \"is_lab\": false,\n" +
+                "            \"is_open\": true,\n" +
+                "            \"location\": \"SHAL 316\",\n" +
+                "            \"name\": \"PRINCIPLES OF ACCOUNTING I\",\n" +
+                "            \"number\": 201,\n" +
+                "            \"open_seats\": 1,\n" +
+                "            \"section\": \"A\",\n" +
+                "            \"semester\": \"2023_Fall\",\n" +
+                "            \"subject\": \"ACCT\",\n" +
+                "            \"times\": [\n" +
+                "                {\n" +
+                "                    \"day\": \"T\",\n" +
+                "                    \"end_time\": \"16:45:00\",\n" +
+                "                    \"start_time\": \"15:30:00\"\n" +
+                "                },\n" +
+                "                {\n" +
+                "                    \"day\": \"R\",\n" +
+                "                    \"end_time\": \"16:45:00\",\n" +
+                "                    \"start_time\": \"15:30:00\"\n" +
+                "                }\n" +
+                "            ],\n" +
+                "            \"total_seats\": 30\n" +
+                "        }";
+
+        // MWF
+        String json2 = "{\n" +
+                "            \"credits\": 3,\n" +
+                "            \"faculty\": [\n" +
+                "                \"Stone, Jennifer Nicole\"\n" +
+                "            ],\n" +
+                "            \"is_lab\": false,\n" +
+                "            \"is_open\": true,\n" +
+                "            \"location\": \"SHAL 114\",\n" +
+                "            \"name\": \"DATA ANALYTICS FOR ACCOUNTING\",\n" +
+                "            \"number\": 310,\n" +
+                "            \"open_seats\": 9,\n" +
+                "            \"section\": \"A\",\n" +
+                "            \"semester\": \"2023_Fall\",\n" +
+                "            \"subject\": \"ACCT\",\n" +
+                "            \"times\": [\n" +
+                "                {\n" +
+                "                    \"day\": \"M\",\n" +
+                "                    \"end_time\": \"09:50:00\",\n" +
+                "                    \"start_time\": \"09:00:00\"\n" +
+                "                },\n" +
+                "                {\n" +
+                "                    \"day\": \"W\",\n" +
+                "                    \"end_time\": \"09:50:00\",\n" +
+                "                    \"start_time\": \"09:00:00\"\n" +
+                "                },\n" +
+                "                {\n" +
+                "                    \"day\": \"F\",\n" +
+                "                    \"end_time\": \"09:50:00\",\n" +
+                "                    \"start_time\": \"09:00:00\"\n" +
+                "                }\n" +
+                "            ],\n" +
+                "            \"total_seats\": 25\n" +
+                "        }";
+
+        // TR
+        String json3 = "{\n" +
+                "            \"credits\": 3,\n" +
+                "            \"faculty\": [\n" +
+                "                \"Shultz, Tricia Michele\"\n" +
+                "            ],\n" +
+                "            \"is_lab\": false,\n" +
+                "            \"is_open\": true,\n" +
+                "            \"location\": \"SHAL 308\",\n" +
+                "            \"name\": \"ADVANCED ACCOUNTING II\",\n" +
+                "            \"number\": 402,\n" +
+                "            \"open_seats\": 10,\n" +
+                "            \"section\": \"A\",\n" +
+                "            \"semester\": \"2023_Fall\",\n" +
+                "            \"subject\": \"ACCT\",\n" +
+                "            \"times\": [\n" +
+                "                {\n" +
+                "                    \"day\": \"T\",\n" +
+                "                    \"end_time\": \"10:45:00\",\n" +
+                "                    \"start_time\": \"09:30:00\"\n" +
+                "                },\n" +
+                "                {\n" +
+                "                    \"day\": \"R\",\n" +
+                "                    \"end_time\": \"10:45:00\",\n" +
+                "                    \"start_time\": \"09:30:00\"\n" +
+                "                }\n" +
+                "            ],\n" +
+                "            \"total_seats\": 25\n" +
+                "        }";
+        String[] js = {json1,json2,json3};
+        for (String j: js) {
+            sampleClasses.add(new Gson().fromJson(j,Class.class));
+        }
+
+
+        Filter f = new TimeFilter("15:00:00","17:30:00");
+        ArrayList<Class> results = f.filt(sampleClasses);
+        assertEquals(1,results.size());
+        assertEquals("PRINCIPLES OF ACCOUNTING I",results.get(0).getName());
     }
 
     @Test
