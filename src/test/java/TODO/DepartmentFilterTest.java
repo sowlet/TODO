5b4586f3656e97bd79435d593e46119648945cb2
bennalex2014 <alexander.j.bennett3@gmail.com>
2025-03-11 17:35:24 -0400

    additional departmentFilter tests

--- src/test/java/TODO/DepartmentFilterTest.java
+++ src/test/java/TODO/DepartmentFilterTest.java
@@ -82,4 +82,77 @@ class DepartmentFilterTest {
         assertEquals("COURSE 2", results.get(0).getName());
         assertEquals("COURSE 3", results.get(1).getName());
     }
+
+    @Test
+    void emptyFilt() {
+        ArrayList<Class> sampleClasses = new ArrayList<>();
+
+        // dummy data -> parody of json
+
+        // course 1 -> accounting
+        String json1 = "{\n" +
+                "            \"credits\": 0,\n" +
+                "            \"faculty\": [\n" +
+                "                \"Inman, John G.\"\n" +
+                "            ],\n" +
+                "            \"is_lab\": false,\n" +
+                "            \"is_open\": false,\n" +
+                "            \"location\": \"Off Campus Course\",\n" +
+                "            \"name\": \"COURSE 1\",\n" +
+                "            \"number\": 300,\n" +
+                "            \"open_seats\": 0,\n" +
+                "            \"section\": \"A\",\n" +
+                "            \"semester\": \"2023_Fall\",\n" +
+                "            \"subject\": \"ACCT\",\n" +
+                "            \"times\": [],\n" +
+                "            \"total_seats\": 0\n" +
+                "        }";
+
+        // course 2 -> computer science
+        String json2 = "{\n" +
+                "            \"credits\": 0,\n" +
+                "            \"faculty\": [\n" +
+                "                \"Inman, John G.\"\n" +
+                "            ],\n" +
+                "            \"is_lab\": false,\n" +
+                "            \"is_open\": false,\n" +
+                "            \"location\": \"Off Campus Course\",\n" +
+                "            \"name\": \"COURSE 2\",\n" +
+                "            \"number\": 300,\n" +
+                "            \"open_seats\": 0,\n" +
+                "            \"section\": \"A\",\n" +
+                "            \"semester\": \"2023_Fall\",\n" +
+                "            \"subject\": \"COMP\",\n" +
+                "            \"times\": [],\n" +
+                "            \"total_seats\": 0\n" +
+                "        }";
+
+        // course 3 -> also computer science
+        String json3 = "{\n" +
+                "            \"credits\": 0,\n" +
+                "            \"faculty\": [\n" +
+                "                \"Inman, John G.\"\n" +
+                "            ],\n" +
+                "            \"is_lab\": false,\n" +
+                "            \"is_open\": false,\n" +
+                "            \"location\": \"off Campus Course\",\n" +
+                "            \"name\": \"COURSE 3\",\n" +
+                "            \"number\": 300,\n" +
+                "            \"open_seats\": 0,\n" +
+                "            \"section\": \"A\",\n" +
+                "            \"semester\": \"2023_Fall\",\n" +
+                "            \"subject\": \"COMP\",\n" +
+                "            \"times\": [],\n" +
+                "            \"total_seats\": 0\n" +
+                "        }";
+        String[] js = {json1,json2,json3};
+        for (String j: js) {
+            sampleClasses.add(new Gson().fromJson(j,Class.class));
+        }
+
+        Filter f = new DepartmentFilter("HUMA");
+        ArrayList<Class> results = f.filt(sampleClasses);
+
+        assertEquals(0, results.size());
+    }
 }
