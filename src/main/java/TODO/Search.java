dafd1530a09a56f4b6dc22729ba9930b3df4cb40
bennalex2014 <alexander.j.bennett3@gmail.com>
2025-03-11 21:26:32 -0400

    implementing removeFilter and modifyFilter

--- src/main/java/TODO/Search.java
+++ src/main/java/TODO/Search.java
@@ -47,7 +47,7 @@ public class Search {
 
 
 
-    /* redundant method -> search can be used to modify the query
+    /* redundant method -> use search to modify the query
     public ArrayList<Class> modifyQuery(String query, ArrayList<Class> classes) {
         //TODO: Implement modifyQuery
         return null;
@@ -56,14 +56,54 @@ public class Search {
 
 
     /*
-    adds Filter object to filters ArrayList, replacing Filter subclasses if there are duplicate types
+    adds Filter object to filters ArrayList, replacing a current filter if there are duplicate types
 
     @param filter the Filter object to be added
     @return new results with updated Filters
      */
     public ArrayList<Class> modifyFilter(Filter filter){
-        //TODO: Implement modifyFilter
-        return null;
+        boolean added = false;
+
+        for (int i = 0; i < filters.size(); i++) { // go through filters
+            if (filter.getClass().equals(filters.get(i).getClass())) { // find duplicate types
+                filters.remove(filters.get(i)); // remove old filter
+                filters.add(filter); // store new filter
+                added = true;
+            }
+        }
+
+        if (!added) {
+            filters.add(filter);
+        }
+
+        filterResults = searchResults;
+
+        for (Filter f: filters) {
+            filterResults = f.filt(filterResults);
+        }
+
+        return filterResults;
     }
 
+    /*
+    removes a filter specified by the type of the Filter object param if it is present
+
+    @param filter object specifying the Filter type to be removed
+    @return List of results without the removed filter object if it was removed
+     */
+    public ArrayList<Class> removeFilter(Filter filter) {
+        for (int i = 0; i < filters.size(); i++) { // go through filters
+            if (filter.getClass().equals(filters.get(i).getClass())) { // find duplicate types
+                filters.remove(filters.get(i)); // remove old filter
+            }
+        }
+
+        filterResults = searchResults;
+
+        for (Filter f: filters) {
+            filterResults = f.filt(filterResults);
+        }
+
+        return filterResults;
+    }
 }
