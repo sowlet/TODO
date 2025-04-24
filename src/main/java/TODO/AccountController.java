a3ba78c8cc881ed1812f15b6fab7c35b07d88f54
karlyRip <126495304+karlyRip@users.noreply.github.com>
2025-04-23 23:00:36 -0400

    The getSchedule method is working, so the route should also be working.

--- src/main/java/TODO/AccountController.java
+++ src/main/java/TODO/AccountController.java
@@ -18,13 +18,6 @@ public class AccountController {
         app.post("/sign-in", this::signUp);
         app.get("/profile", this::getMajorAndMinor);
         app.post("/profile", this::updateMajorAndMinor);
-
-
-        app.get("/schedule", this::getAllSchedules);
-        app.post("/schedule", this::addSchedule);
-        app.delete("/schedule", this::deleteSchedule);
-
-
     }
 
     private void validateLogin(Context con){
@@ -42,29 +35,6 @@ public class AccountController {
         Boolean addAccountResult = dm.addAccountToDatabase(username, password, email);
 
         con.json(addAccountResult);
-    }
-
-    private void getAllSchedules(Context con){
-        String username = con.queryParam("username");
-        ResultSet addScheduleResult = dm.getSchedules(username);
-
-        con.json(addScheduleResult);
-    }
-
-    private void addSchedule(Context con){
-        String username = con.queryParam("username");
-        String scheduleName = con.queryParam("name");
-        Boolean addScheduleResult = dm.addScheduleToDatabase(username, scheduleName);
-
-        con.json(addScheduleResult);
-    }
-
-    private void deleteSchedule(Context con){
-        String username = con.queryParam("username");
-        String scheduleName = con.queryParam("name");
-        Boolean deleteScheduleResult = dm.removeScheduleFromDatabase(username, scheduleName);
-
-        con.json(deleteScheduleResult);
     }
 
     private void updateMajorAndMinor(Context con){
