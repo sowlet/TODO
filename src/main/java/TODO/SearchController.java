736b17d5c3ea1c3d6ce201922aea1ed31a453fa5
karlyRip <126495304+karlyRip@users.noreply.github.com>
2025-04-24 23:41:08 -0400

    Fixed search route output.

--- src/main/java/TODO/SearchController.java
+++ src/main/java/TODO/SearchController.java
@@ -1,6 +1,8 @@
 package TODO;
 
 import com.google.gson.JsonArray;
+import com.google.gson.JsonElement;
+import com.google.gson.JsonObject;
 import io.javalin.Javalin;
 import io.javalin.http.Context;
 
@@ -23,26 +25,45 @@ public class SearchController {
         String query = con.queryParam("query");
         JsonArray res = dm.search(query);
 
-        ArrayList<Map<String, Object>> resultList = new ArrayList<>();
-        for (int i = 0; i < res.size(); i++) {
-            Map<String, Object> map = new HashMap<>();
-            res.get(i).getAsJsonObject().entrySet().forEach(entry -> {
-                if (entry.getValue().isJsonArray()) {
-                    // Convert JsonArray to a Map
-                    Map<String, Object> nestedMap = new HashMap<>();
-                    JsonArray jsonArray = entry.getValue().getAsJsonArray();
-                    for (int j = 0; j < jsonArray.size(); j++) {
-                        nestedMap.put(String.valueOf(j), jsonArray.get(j).getAsString());
-                    }
-                    map.put(entry.getKey(), nestedMap);
-                } else {
-                    map.put(entry.getKey(), entry.getValue().getAsString());
-                }
-            });
-            resultList.add(map);
+        if (res == null || res.size() == 0) {
+            con.result("No classes found.");
+            if(res != null){
+                System.out.println(res.size());
+            }
+            return;
         }
 
-        con.json(resultList);
+        // Create a list to store the maps
+        Map<String, Object> map = new HashMap<>();
+        // Iterate through the JsonArray
+        for (JsonElement element : res) {
+            JsonObject jsonObject = element.getAsJsonObject();
+
+            // Populate the map with key-value pairs from the JsonObject
+            for (Map.Entry<String, JsonElement> entry : jsonObject.entrySet()) {
+                if (entry.getValue().isJsonObject()) {
+                    map.put(entry.getKey(), entry.getValue().getAsJsonObject().toString());
+                } else if (entry.getValue().isJsonArray()) {
+                    JsonArray timeArray = entry.getValue().getAsJsonArray();
+                    String days = "";
+                    String start_time = timeArray.get(0).getAsJsonObject().get("startTime").getAsString();
+                    String end_time = timeArray.get(0).getAsJsonObject().get("endTime").getAsString();
+
+                    for (JsonElement timeElement : timeArray) {
+                        JsonObject timeObj = timeElement.getAsJsonObject();
+                        days += timeObj.get("days").getAsString();
+                    }
+
+                    map.put("days", days);
+                    map.put("startTime", start_time);
+                    map.put("endTime", end_time);
+                } else {
+                    map.put(entry.getKey(), entry.getValue().toString());
+                }
+            }
+        }
+
+        con.json(map);
     }
 
 }
